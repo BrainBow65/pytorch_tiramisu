89180bbc4ada85f00780333a9322d42c650f806d
Ole Bialas <ole.bialas@posteo.de>
2025-07-08 10:49:29 +0200

    quantize annotated images

--- data_retrieval_Ivygap.py
+++ data_retrieval_Ivygap.py
@@ -1,13 +1,14 @@
-import os
+from pathlib import Path
 import requests
 
 # Set up the base URL for the API
 BASE_URL = "http://api.brain-map.org/api/v2/data/query.json"
 
-# Define the directory to save images
-SAVE_DIR_HE = "D:/IVY_GAP dataset/H&Es"
-SAVE_DIR_ANNOTATED = 'D:/IVY_GAP dataset/Annotated'
-# os.makedirs(SAVE_DIR, exist_ok=True)
+SAVE_DIR = Path("Ivy_gap_dataset")
+SAVE_DIR_HE = SAVE_DIR / "H&Es"
+SAVE_DIR_ANNOTATED = SAVE_DIR / "Annotated"
+SAVE_DIR_HE.mkdir(parents=True, exist_ok=True)
+SAVE_DIR_ANNOTATED.mkdir(parents=True, exist_ok=True)
 
 
 def fetch_specimen_ids():
@@ -15,7 +16,7 @@ def fetch_specimen_ids():
     url = f"{BASE_URL}?criteria=model::Specimen,rma::criteria,specimen_types[name$eq'Anatomic Structures ISH Survey'],rma::options[num_rows$eqall]"
     response = requests.get(url)
     data = response.json()
-    return [specimen['external_specimen_name'] for specimen in data['msg']]
+    return [specimen["external_specimen_name"] for specimen in data["msg"]]
 
 
 def fetch_section_data_sets(specimen_id):
@@ -23,7 +24,7 @@ def fetch_section_data_sets(specimen_id):
     url = f"{BASE_URL}?criteria=model::SectionDataSet,rma::criteria,specimen[external_specimen_name$eq'{specimen_id}'],treatments[name$eq'H%26E'],rma::include,sub_images"
     response = requests.get(url)
     data = response.json()
-    return data['msg']
+    return data["msg"]
 
 
 def download_image(image_id, view_type, save_path):
@@ -35,7 +36,7 @@ def download_image(image_id, view_type, save_path):
 
     response = requests.get(image_url)
     if response.status_code == 200:
-        with open(save_path, 'wb') as f:
+        with open(save_path, "wb") as f:
             f.write(response.content)
         print(f"Downloaded {view_type} image to {save_path}")
     else:
@@ -48,14 +49,20 @@ def main():
     for specimen_id in specimen_ids:
         section_data_sets = fetch_section_data_sets(specimen_id)
         for section in section_data_sets:
-            for sub_image in section.get('sub_images', []):
-                image_id = sub_image['id']
+            for sub_image in section.get("sub_images", []):
+                image_id = sub_image["id"]
                 # Download H&E image
-                download_image(image_id, None,
-                               os.path.join(SAVE_DIR_HE, f"{specimen_id}_HE_{image_id}.jpg"))
+                download_image(
+                    image_id,
+                    None,
+                    SAVE_DIR_HE / f"{specimen_id}_HE_{image_id}.jpg",
+                )
                 # Download TumorFeatureAnnotation image
-                download_image(image_id, 'tumor_feature_annotation',
-                               os.path.join(SAVE_DIR_ANNOTATED, f"{specimen_id}_TFA_{image_id}.jpg"))
+                download_image(
+                    image_id,
+                    "tumor_feature_annotation",
+                    SAVE_DIR_ANNOTATED / f"{specimen_id}_TFA_{image_id}.jpg",
+                )
 
 
 if __name__ == "__main__":
--- resize_ivy_gap.py
+++ resize_ivy_gap.py
@@ -1,7 +1,20 @@
 from pathlib import Path
+import numpy as np
 from PIL import Image
-Image.MAX_IMAGE_PIXELS = None
 
+Image.MAX_IMAGE_PIXELS = None
+SIZE = (1024, 1024)
+
+# Get RGB values for the different colors in the annotation
+rgb_values = np.loadtxt("colors.csv", delimiter=",", dtype=str)
+rgb_values = rgb_values[:, :3].astype(int)
+n_colors = len(rgb_values)
+# Make a color palette
+palette = list(rgb_values.flatten())
+palette += [255, 255, 255]  # add white
+palette += [0, 0, 0] * (256 - n_colors - 1)  # pad to 256 colors
+palette_img = Image.new("P", (1, 1))
+palette_img.putpalette(palette)
 
 DIR_ANNOTATED = Path("Ivy_gap_dataset/Annotated")
 DIR_HE = Path("Ivy_gap_dataset/HE")
@@ -12,7 +25,12 @@ DIR_RESIZED_HE.mkdir(exist_ok=True)
 DIR_RESIZED_ANNOTATED = Path(DIR_ANNOTATED / "Resized_annotated")
 DIR_RESIZED_ANNOTATED.mkdir(exist_ok=True)
 
-for file in DIR_ANNOTATED.glob("*.jpg"):
-    image = Image.open(file)
-    image = image.resize((1024, 1024),resample=Image.NEAREST)
-    image.save(DIR_RESIZED_ANNOTATED/file.name)
+for f_annotated, f_he in zip(DIR_ANNOTATED.glob("*.jpg"), DIR_HE.glob("*.jpg")):
+    image_annotated = Image.open(f_annotated)
+    image_annotated = image_annotated.resize(SIZE, resample=Image.NEAREST)
+    image_annotated = image_annotated.quantize(palette=palette_img).convert("RGB")
+    image_annotated.save(DIR_RESIZED_ANNOTATED / f_annotated.name)
+
+    image_he = Image.open(f_he)
+    image_he = image_he.resize(SIZE, resample=Image.NEAREST)
+    image_he.save(DIR_RESIZED_ANNOTATED / f_he.name)
