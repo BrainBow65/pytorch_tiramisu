32af484f0eb5ed4c300ccd6756e0387843e1dd50
Michelle Latham <michellelatham740@gmail.com>
2025-07-16 11:29:00 +0200

    created dataset class and train_ivy_gap notebook

---
+++ datasets/ivy_gap.py
@@ -0,0 +1,62 @@
+import os
+from pathlib import Path
+import torch
+import torch.utils.data as data
+import numpy as np
+from PIL import Image
+from torchvision.datasets.folder import is_image_file, default_loader
+
+classes = ["GBM", "Leading Edge Region", 
+           "Hyperplastic blood vessels", 
+           "infiltrating tumor region", 
+           "Cellular tumor region", 
+           "Perinecrotic zone", 
+           "Pseudopalisading cells but no visible necrosis",
+           "Pseudopalisading cells around necrosis",
+           "Microvascular Proliferation",
+           "Necrosis"]
+
+class_color = [(0, 114, 201),
+               (33, 143, 165),
+               (255, 102, 0),
+               (209, 4, 208),
+               (5, 208, 4),
+               (67, 209, 248),
+               (5, 4, 208),
+               (5, 208, 170),
+               (255, 51, 0),
+               (6, 5, 5)]
+
+
+class IvyGap(data.Dataset):
+    def __init__(self, root, split='train', joint_transform=None,
+                 transform=None, target_transform=None,
+                 loader=default_loader):
+        self.root = Path(root)
+        assert split in ('train', 'val', 'test')
+        self.split = split
+        self.joint_transform = joint_transform
+        self.transform = transform
+        self.target_transform = target_transform
+        self.loader = loader
+        self.imgs = list((self.root/self.split).glob("*.jpg"))
+
+    def __getitem__(self, index):
+        path = str(self.imgs[index])
+        img = self.loader(path)
+        target_path = path.replace(self.split, self.split + 'annot')
+        target_path = target_path.replace("HE", "TFA")
+        target = self.loader(target_path)  # Image.open ?
+
+
+        if self.joint_transform is not None:
+            img, target = self.joint_transform([img, target])
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        # target = self.target_transform(target)
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
